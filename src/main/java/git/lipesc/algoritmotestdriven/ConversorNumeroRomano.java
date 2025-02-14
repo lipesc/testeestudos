8b6456d369f34d9ca97ea03665bf9e0251c4c3c3
lipesc <felipedrhouse@hotmail.com>
2025-02-14 15:18:55 -0300

    rafarorado: key do map para char e metodo converte para interar sobre o map com charAt recebendo o index i, adicionar a um contador quando encontrar um romano correspondente e retornar esse valor.

--- src/main/java/git/lipesc/algoritmotestdriven/ConversorNumeroRomano.java
+++ src/main/java/git/lipesc/algoritmotestdriven/ConversorNumeroRomano.java
@@ -5,20 +5,24 @@ import java.util.Map;
 
 public class ConversorNumeroRomano {
 
-  private static Map<String, Integer> tabela = new HashMap<>() {
+  private static Map<Character, Integer> tabela = new HashMap<>() {
     {
-      put("I", 1);
-      put("V", 5);
-      put("X", 10);
-      put("L", 50);
-      put("C", 100);
-      put("D", 500);
-      put("M", 1000);
+      put('I', 1);
+      put('V', 5);
+      put('X', 10);
+      put('L', 50);
+      put('C', 100);
+      put('D', 500);
+      put('M', 1000);
     }
   };
 
   public int converte(String numeroEmRomano) {
-    return tabela.get(numeroEmRomano);
+    int count = 0;
+    for (int i = 0; i < numeroEmRomano.length(); i++) {
+      count += tabela.get(numeroEmRomano.charAt(i));
+    }
+    return count;
   }
 
 }
