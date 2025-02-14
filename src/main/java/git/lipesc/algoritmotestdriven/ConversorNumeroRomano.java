a441143dd5540f6a45e1f12812fc52612f1d96b7
lipesc <felipedrhouse@hotmail.com>
2025-02-14 15:04:10 -0300

    implementando um map para cobrir todos casos mais facil

--- src/main/java/git/lipesc/algoritmotestdriven/ConversorNumeroRomano.java
+++ src/main/java/git/lipesc/algoritmotestdriven/ConversorNumeroRomano.java
@@ -1,6 +1,22 @@
 package git.lipesc.algoritmotestdriven;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConversorNumeroRomano {
+
+  private static Map<String, Integer> tabela = new HashMap<>() {
+    {
+      put("I", 1);
+      put("V", 5);
+      put("X", 10);
+      put("L", 50);
+      put("C", 100);
+      put("D", 500);
+      put("M", 1000);
+    }
+  };
+
   public int converte(String numeroEmRomano) {
 
     if (numeroEmRomano.equals("I"))
